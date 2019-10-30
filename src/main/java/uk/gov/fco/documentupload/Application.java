fce61ea061767ca7f98db36c4678eca1fffe6c76
Max Stewart <max@cautionyourblast.com>
2019-10-30 21:09:05 +0000

    Adding MIT license.

--- src/main/java/uk/gov/fco/documentupload/Application.java
+++ src/main/java/uk/gov/fco/documentupload/Application.java
@@ -1,10 +1,8 @@
 package uk.gov.fco.documentupload;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -19,11 +17,11 @@ public class Application {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes("oauth2Scheme",
-                                new SecurityScheme().type(SecurityScheme.Type.OAUTH2).scheme("oauth2")))
-                .info(new Info().title("Document Upload").version("X.X.X").description("")
-                        .termsOfService("http://swagger.io/terms/")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+                .info(new Info().title("Document Upload")
+                        .version("X.X.X")
+                        .description("Service to check files for viruses nad store them.")
+                        .license(new License()
+                                .name("MIT")
+                                .url("https://raw.githubusercontent.com/CautionYourBlast/document-uploaa/master/LICENSE")));
     }
 }
