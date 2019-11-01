9f1cb4b05eb944881a94da456e64cc0c0d0f59dc
Max Stewart <max@cautionyourblast.com>
2019-11-01 09:40:52 +0000

    Change antivirus properties prefix..

--- src/main/java/uk/gov/fco/documentupload/config/AntiVirusProperties.java
+++ src/main/java/uk/gov/fco/documentupload/config/AntiVirusProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "clamav")
+@ConfigurationProperties(prefix = "antivirus")
 public class AntiVirusProperties {
 
     private String host;
