6f88c4ee7caf980215eb54b3c9a8c8d60a0b4eb2
Ali Salaman <ali@cautionyourblast.com>
2024-06-14 12:20:32 +0100

    added conditional to set endpoint configuration when building AmazonS3 object in dev environment. Fails fast when invalid content type detected.

--- src/main/java/uk/gov/fco/documentupload/service/storage/S3StorageClient.java
+++ src/main/java/uk/gov/fco/documentupload/service/storage/S3StorageClient.java
@@ -6,9 +6,13 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.S3Object;
 import lombok.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
+import uk.gov.fco.documentupload.config.EnvironmentUtil;
 
 import java.io.InputStream;
 
@@ -16,19 +20,28 @@ import java.io.InputStream;
 @ConditionalOnProperty(name = "storage.engine", havingValue = "s3")
 public class S3StorageClient extends StorageClient {
 
+    private static final Logger log = LoggerFactory.getLogger(S3StorageClient.class);
+
     private String bucket;
 
     private AmazonS3 amazonS3;
 
-    public S3StorageClient(@Value("${storage.s3.bucket}") @NonNull String bucket) {
+    public S3StorageClient(@Value("${storage.s3.bucket}") @NonNull String bucket, Environment environment) {
         this.bucket = bucket;
-        String endpoint = System.getenv("AWS_ENDPOINT");
-        String region = System.getenv("AWS_REGION");
 
-        amazonS3 = AmazonS3ClientBuilder.standard()
-            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region))
-            .withPathStyleAccessEnabled(true)
-            .build();
+        if (EnvironmentUtil.isDevelopment(environment)) {
+            log.info("Using S3 storage in development mode");
+            String endpoint = System.getenv("AWS_ENDPOINT");
+            String region = System.getenv("AWS_REGION");
+
+            amazonS3 = AmazonS3ClientBuilder.standard()
+                    .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region))
+                    .withPathStyleAccessEnabled(true)
+                    .build();
+        } else {
+            log.info("Using S3 storage in non-development mode");
+            amazonS3 = AmazonS3ClientBuilder.defaultClient();
+        }
     }
 
     @Override
